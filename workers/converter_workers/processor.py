8566dba95e2c96a163a985052ef231f92cbd982f
Michael Madu <ebukamic1111@outlook.com>
2026-06-30 06:42:41 -0300

    feat migrated dev processor function to test/integrations/utils

--- workers/converter_workers/processor.py
+++ workers/converter_workers/processor.py
@@ -75,43 +75,6 @@ async def process_job(context: WorkerContext, job: ConversionJob) -> None:
         raise RuntimeError(error_message)
 
 
-
-#for Test purposes
-def dev_process_job(context: WorkerContext, job: ConversionJob) -> None:
-    log_context = context.get_log_context(job.job_id, job.conversion)
-    worker_logger.info(f"starting dev processing for {job.job_id} with conversion {job.conversion}")
-
-    try:
-        job.start_processing()
-        worker_logger.debug(f"job status updated to processing for job {job.job_id}")
-
-
-        dir = Path("converter_storage")
-        input_file, output_file = resolve_path(job.input_file, job.conversion, dir)
-
-        context.storage_port.download(job.input_file, input_file)
-        worker_logger.debug(f"Downloaded input file for job {job.job_id} to {input_file}", extra=log_context)
-
-        converter = context.converter_registry.get_converter(job.conversion)
-        if not converter:
-            raise RuntimeError(f"No converter found for conversion type {job.conversion}")
-        
-        converter(str(input_file), str(output_file))
-        worker_logger.debug(f"Conversion completed for job {job.job_id}, output at {output_file}", extra=log_context)
-
-        output_dest = f"s3-file_store/{output_file.name}"
-        context.storage_port.upload(output_dest, output_file)
-
-        worker_logger.debug(f"Uploaded output file for job {job.job_id} to {output_dest}", extra=log_context)
-
-            # Update job status to COMPLETED
-        job.complete(output_dest)
-
-    except Exception as e:
-        error_message = str(e)
-        job.fail(error_message)
-        raise RuntimeError(error_message)
-
 def resolve_path(
     file_location: str,
     conversion: ConversionType,
